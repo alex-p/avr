ea4c2aa631edb664228e5d44046e865d00e816a2
Alex Prevoteau <alex.prevoteau@gmail.com>
2015-08-02 15:43:17 -0700

    Adding fresh Xcode 6 project (iPhone only)

---
+++ AugmentedVirtualReality/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AugmentedVirtualReality
+//
+//  Created by Alex Prevoteau on 8/2/15.
+//  Copyright (c) 2015 Alex Prevoteau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
